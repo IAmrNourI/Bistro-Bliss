cde52a068e678c14c7efbc146c49457acbfa6483
fathy61 <ahmedfathy15993@gmail.com>
2024-12-09 17:33:22 +0200

    f v1.1

---
+++ client/src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+
+      <>
+        <section className='hero'>
+          <div className="hero-content text-center">
+            <h1 className='hero-title'>Best food for your taste</h1>
+            <p className='hero-info'>Discover delectable cuisine and unforgettable moments in our welcoming, culinary haven.</p>
+            <div className='btns-hero'>
+              <button className='bt book'>Book A Table</button>
+              <button className='bt menu'>Explore Menu</button>
+            </div>
+          </div>
+        </section>
+      </>
+        
+  )
+}
