645f30c84deabb251f0d3df349117bc1a08411de
fathy61 <ahmedfathy15993@gmail.com>
2024-12-27 19:08:37 +0200

    fv2.3

--- client/src/Components/Admin/Admin.jsx
+++ client/src/Components/Admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from '../../assets/logo.png'
 import userpic from "../../assets/userpic.jpg";
+import { Link } from 'react-router-dom';
 
 export default function Admin() {
 
@@ -8,29 +9,27 @@ return (
 
     <>
         <section className='Admin'>
-            <div className="row">
-                <div className="col-3"> 
-                    <div className="admin-panal bg-dark-subtle p-3 vh-100">
-                        <div className="logo d-flex align-items-center">
-                            {/* <img src={logo} width="40px" alt="logo" />
-                            <h4 className='ms-2 mt-2'>Bistro Bliss</h4> */}
-                        </div>
-                        <div className="details d-flex">
-                        <img width="70px" className="rounded-circle" src={userpic} alt="userpic"/>
-                            <div className='d-flex flex-column ms-3'>
-                                <span className='text-secondary mt-1'>Welcome</span>
-                                <span className='mt-2 fw-500'>fathy_abdelhamind</span>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-2 ps-0"> 
+                        <div className="admin-panal bg-dark-subtle p-3 vh-100">
+                            <div className="details d-flex br">
+                            <img width="70px" className="rounded-circle" src={userpic} alt="userpic"/>
+                                <div className='d-flex flex-column ms-3'>
+                                    <span className='text-secondary mt-1'>Welcome</span>
+                                    <span className='mt-2 fw-500 '>Amr Nour</span>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="side-nav">
-                            <ul>
-                                <li>Munu</li>
-                                <li>Contact</li>
-                                <li>Booking</li>
-                                <li>Users</li>
-                            </ul>
-                            
+                            <div className="side-nav mt-3">
+                                <ul className='p-0'>
+                                    <li> <Link to="menu" className=''>Menu</Link></li>
+                                    <li>Contact</li>
+                                    <li>Booking</li>
+                                    <li>Users</li>
+                                </ul>
+                                
+                            </div>
                         </div>
                     </div>
                 </div>
