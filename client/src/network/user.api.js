4c2fa5fd2bcb8af5efad225a29733a283cc7652d
Amr Nour <amr.nour.eng@gmail.com>
2024-12-27 03:23:50 +0200

    remove edits

--- client/src/network/user.api.js
+++ client/src/network/user.api.js
@@ -1,75 +1,67 @@
-import { axiosInstance } from "./index";
+import { axiosInstance } from "./index"
 
-const register = async (data) => {
-  return await axiosInstance.post("/user/register", data);
-};
 
-const verify = async (data) => {
-  // >> verify-otp
-  return await axiosInstance.post("/user/verify-otp", data);
-};
+const register = async(data) => {
+    return await axiosInstance.post("/user/register", data)
+}
 
-const resendOtp = async (data) => {
-  return await axiosInstance.post("/user/resend-otp", data);
-};
+const verify = async(data) => { // >> verify-otp
+    return await axiosInstance.post("/user/verify-otp", data)
+}
 
-const email = async (data) => {
-  return await axiosInstance.post("/user/email", data);
-};
+const resendOtp = async(data) => {
+    return await axiosInstance.post("/user/resend-otp", data)
+}
 
-const password = async (data) => {
-  return await axiosInstance.post("/user/password", data, {
-    withCredentials: true,
-  });
-};
+const email = async(data) => {
+    return await axiosInstance.post("/user/email", data)
+}
 
-const items = async () => {
-  return await axiosInstance.get("/menu/");
-};
+const password = async(data) => {
+    return await axiosInstance.post("/user/password", data)
+}
 
-const addItem = async (data) => {
-  // console.log(localStorage.getItem("userToken"))
-  // return await axiosInstance.post("/menu/add-item", data)
-  const token = localStorage.getItem("userToken");
-  return await axiosInstance.post("/menu/add-item", data, {
+const items = async() => {
+    return await axiosInstance.get("/menu/")
+}
+
+const addItem = async(data) => {
+    // console.log(localStorage.getItem("userToken"))
+    // return await axiosInstance.post("/menu/add-item", data)
+    const token = localStorage.getItem("userToken");    
+    return await axiosInstance.post("/menu/add-item", data, {
+        
     headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + token,
     },
-    withCredentials: true,
-  });
-};
+});
+}
 
 const deleteItem = async (data) => {
-  const token = localStorage.getItem("userToken");
-  return await axiosInstance.delete("/menu/delete-item", {
-    data: data,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-  });
-};
+    const token = localStorage.getItem("userToken");
+    return await axiosInstance.delete("/menu/delete-item", {
+        data: data,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+        },
+    });
+}
 
 const editItem = async (data) => {
-  // return await axiosInstance.put("/menu/edit-item", data)
-  const token = localStorage.getItem("userToken");
-  return await axiosInstance.put("/menu/edit-item", data, {
+    // return await axiosInstance.put("/menu/edit-item", data)
+    const token = localStorage.getItem("userToken");    
+    return await axiosInstance.put("/menu/edit-item", data, {
+        
     headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + token,
     },
-  });
-};
+});
 
-export {
-  register,
-  verify,
-  resendOtp,
-  email,
-  password,
-  items,
-  addItem,
-  deleteItem,
-  editItem,
-};
+}
+
+
+
+export { register, verify, resendOtp, email, password, items, addItem, deleteItem, editItem }
